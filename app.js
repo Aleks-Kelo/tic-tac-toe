01a7b5f6e758ecee9d2600249c0f5a7bbec974e8
Aleks-Kelo <60621181+Aleks-Kelo@users.noreply.github.com>
2021-01-01 17:02:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,42 @@
 
-var gamePlacements = document.querySelectorAll(".gamePlace");
-var status = document.querySelector(".")
+let boxes = document.querySelectorAll(".gamePlace");
+let nextPlayer = document.querySelector("#status");
+let reset = document.querySelector("#Reset");
+let changePlayer = document.querySelector("#changePlayer");
 
-var xIsNext=true;
-var gameContinuous =true;
+function move(n){
+
+    n = n.target;
+    if(n.innerHTML == ''){
+        n.innerHTML = nextPlayer.innerHTML;
+        nextPlayer.innerHTML = nextPlayer.innerHTML == 'O' ? 'X' : 'O';
+    }  
+}
+
+reset.addEventListener("click",resetGame);
+
+function resetGame(){
+    boxes.forEach(function (box){
+        box.innerHTML = ""
+    })
+    nextPlayer.innerHTML = "X"
+}
+
+changePlayer.addEventListener("click",playerChanger);
+
+function playerChanger(){
+    if( boxes.forEach()){
+        box.innerHTML == ""
+            nextPlayer.innerHTML = nextPlayer.innerHTML == 'O' ? 'X' : 'O';
+    }
+}
 
 
-function gameWIn
+
+
+
+
+
+boxes.forEach(function (box){
+    box.addEventListener("click", move);
+})
